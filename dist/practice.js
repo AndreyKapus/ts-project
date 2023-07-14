f1934ddbc37e9c3df2567280d4066bc5a9fa3456
AndreyKapus <kapustnikov@ukr.net>
2023-07-14 13:26:28 +0300

    tools

--- dist/practice.js
+++ dist/practice.js
@@ -1,12 +1,2 @@
 "use strict";
-let person;
-person = ['Max', 21];
-var Status;
-(function (Status) {
-    Status[Status["LOADING"] = 0] = "LOADING";
-    Status[Status["READY"] = 1] = "READY";
-})(Status || (Status = {}));
-;
-const setStatus = {
-    status: Status.LOADING
-};
+//# sourceMappingURL=practice.js.map
